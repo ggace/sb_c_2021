c8db0a3f9ae6ad3572322e0251b0f654d67cddc3
ggace <skydream5485@gmail.com>
2021-07-17 23:28:58 +0900

    세팅

---
+++ src/main/java/com/min/exam/demo/controller/UserHomeController.java
@@ -0,0 +1,15 @@
+package com.min.exam.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class UserHomeController {
+	@RequestMapping("usr/home/main") //<- url => 아래 함수 실행 0 
+	@ResponseBody //아래 함수가 response다
+	public String showMain() {
+		return "안녕하세요";
+	}
+
+}
